917e9b26f153bbbe93e6cf2ec9bbb27b500f5f21
mi2428 <mi2428782020@gmail.com>
2018-02-10 20:27:58 +0900

    Rebasing: a lot of minor fixes
    Signed-off-by: mi2428 <mi2428782020@gmail.com>

---
+++ test/validator.py
@@ -0,0 +1,18 @@
+import json, jsonschema
+
+def __validate(data, schema):
+    with open(schema, 'r') as fd:
+        schema = json.load(fd)
+        jsonschema.validate(data, schema)
+
+def validate_members_file():
+    with open('../members.json', 'r') as fd:
+        data = json.load(fd)
+        __validate(data, './members.jsonschema')
+
+def validate_past_members_file():
+    pass
+
+if __name__ == '__main__':
+    validate_members_file()
+    validate_past_members_file()
