1347c988e1d513207680292d3c48de7989ccc750
mi2428 <mi2428782020@gmail.com>
2019-03-12 21:45:26 +0900

    Update validator
    Signed-off-by: mi2428 <mi2428782020@gmail.com>

--- test/validator.py
+++ test/validator.py
@@ -1,26 +1,23 @@
-import json, jsonschema
+import sys
+import glob
+import json
+import jsonschema
 
-def __validate(data, schema):
-    with open(schema, 'r') as fd:
-        schema = json.load(fd)
-        jsonschema.validate(data, schema)
+class Validator:
+    def __init__(self, schema):
+        with open(schema, 'r') as fd:
+            self.schema = json.load(fd)
 
-def validate_members_file():
-    with open('../members.json', 'r') as fd:
-        data = json.load(fd)
-        __validate(data, './members.jsonschema')
+    def check(self, data):
+        with open(data, 'r') as fd:
+            data = json.load(fd)
+            jsonschema.validate(data, self.schema)
 
-def validate_past_students_file():
-    with open('../past_students.json', 'r') as fd:
-        data = json.load(fd)
-        __validate(data, './past_students.jsonschema')
-
-def validate_past_exchange_students_file():
-    with open('../past_exchange_students.json', 'r') as fd:
-        data = json.load(fd)
-        __validate(data, './past_exchange_students.jsonschema')
+members = lambda: Validator('./members.schema.json').check('../members.json')
+past_students = lambda: Validator('./past_students.schema.json').check('../past_students.json')
+past_exchange_students = lambda: Validator('./past_exchange_students.schema.json').check( '../past_exchange_students.json')
 
 if __name__ == '__main__':
-    validate_members_file()
-    validate_past_students_file()
-    validate_past_exchange_students_file()
+    members()
+    past_students()
+    past_exchange_students()
